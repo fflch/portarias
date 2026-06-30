38cc79fbdbc350101fa141b795bd4ce220a629b4
Guilherme Frias <guilherme.frias.o.t3@gmail.com>
2026-06-30 11:14:53 -0300

    [feat]: migration portaria e pré implementação do sistema de status

--- app/Models/Portaria.php
+++ app/Models/Portaria.php
@@ -19,4 +19,49 @@ class Portaria extends Model
         'approved_by',
         'published_at'
     ];
+
+    public function getStatus(){
+        $status = [
+            'em_elaboracao' => [
+                'name' => "Em Elaboração",
+                'optional' => 'Empresa'
+            ],
+            'em_analise_tecnica' => [
+                'name' => "Análise Técnica",
+                'optional' => 'Setor de Graduação'
+            ],
+
+            'assinatura' => [
+                'name' => "Assinatura",
+                'optional' => 'Setor de Graduação'
+            ],
+
+            'em_analise_academica' => [
+                'name' => "Parecer de Mérito",
+                'optional' => 'Docente'
+            ],
+            'concluido' => [
+                'name' => "Concluído",
+                'optional' => 'Docente'
+            ],
+            'em_alteracao' => [
+                'name' => "Aditivo de Alterações",
+                'optional' => 'Empresa'
+            ],
+            'analise_alteracao_parecerista' => [
+                'name' => "Aditivo de Alterações - Análise do Parecerista",
+                'optional' => 'Docente'
+            ],
+            'rescisao' => [
+                'name' => "Rescisão",
+                'optional' => 'Empresa'
+            ],
+            'cancelado' => [
+                'name' => "Cancelado",
+                'optional' => 'Estágio Cancelado'
+            ],
+        ];
+
+        return $status;
+    }
 }
---
+++ database/migrations/2026_06_26_141701_create_portarias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void16
+    {
+        Schema::create("portarias", function(Blueprint $table) {
+            $table->int('number');
+            $table->date('date');
+            $table->enum('type', ['cpp', 'comissao', 'designacao', 'administrativa']);
+            $table->string('status');
+            $table->boolean('is_legacy');
+            $table->foreignId('revokes_id')->nullable();
+            $table->foreignId('created_by');
+            $table->foreignId('approved_by');
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
